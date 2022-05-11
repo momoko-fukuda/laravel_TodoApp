39a0692dc6b80c0999520728d1691d7a2aaa34d6
momoko-fukuda <momo.taka0830@gmail.com>
2022-05-11 12:25:19 +0000

    set routing

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route:: アクション（'接続先', 'どのコントローラーを動かすか'）;
+
+// TODOアプリにアクセスした際に一番最初に表示されるページを指定
+Route::get('/', 'HomeController@index');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource("goals", "GoalController");
+
+Route::resource("goals.todos", "TodoController");
+
+// 作成したTodoを並び替える処理を行うルーティングです。
+Route::post('/goals/{goal}/todos/{todo}/sort', 'TodoController@sort');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
